26189e4fdd6983421145be4962ffedd047dab3ff
SayanShendre22 <sayanshendreedu@gmail.com>
2023-03-08 22:36:19 +0530

    commit by sayan

--- megaproject-frontend/src/App.js
+++ megaproject-frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Sayan bhai op bolte
         </p>
         <a
           className="App-link"
