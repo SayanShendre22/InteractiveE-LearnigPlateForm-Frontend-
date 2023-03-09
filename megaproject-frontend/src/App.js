b2353f7d1f129d0b6a1cf1183f33495c04f4226e
DivyaPathrabe <divyapathrabe02@gmail.com>
2023-03-09 13:04:49 +0530

    changes done by divya

--- megaproject-frontend/src/App.js
+++ megaproject-frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Sayan bhai op bolte
+         Mega Project Front-end
         </p>
         <a
           className="App-link"
